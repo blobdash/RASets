30080cda36a7f40d6d4d72ef88af14958afe396e
blobdash <fruityenloops@gmail.com>
2026-08-16 23:06:30 +0200

    Moon: Progression achievements

--- Moon/logic.py
+++ Moon/logic.py
@@ -5,5 +5,101 @@ from pycheevos.models.leaderboard import Leaderboard
 
 from memory import Memory
 
+class Episode:
+  EPISODE_00 = 0x08
+  EPISODE_01 = 0x14
+  EPISODE_02 = 0x20
+  EPISODE_03 = 0x2c
+  EPISODE_04 = 0x38
+  EPISODE_05 = 0x44
+  EPISODE_06 = 0x50
+  EPISODE_07 = 0x5c
+  EPISODE_08 = 0x68
+  EPISODE_09 = 0x74
+  EPISODE_10 = 0x80
+  EPISODE_11 = 0x8c
+  EPISODE_12 = 0x98
+  EPISODE_13 = 0xa4
+  EPISODE_14 = 0xb0
+  EPISODE_15 = 0xbc
+  EPISODE_16 = 0xc8
+  EPISODE_17 = 0xd4
+
+class Gamemode:
+  ADVENTURE = 0x00
+  QUICKPLAY = 0x01
+  
+
+class Weapon:
+  AMMO: MemoryValue
+  AMMO_RESERVE: MemoryValue | None
+  UNLOCKED: MemoryValue
+
+  def __init__(self, ammoaddr: MemoryValue, reserveaddr: MemoryValue | None, unlockbit: MemorySize) -> None:
+        self.AMMO = ammoaddr
+        self.AMMO_RESERVE = reserveaddr
+        self.UNLOCKED = MemoryValue(Memory.WEAPON_UNLOCKS.address, unlockbit)
+  
+  def unlocked(self, episode: int):
+    return group(
+      (Memory.CURRENT_EPISODE == episode),
+      (Memory.PLAY_STATE == 0x00),
+      (Memory.CURRENT_GAMEMODE == 0x00),
+      delta(self.UNLOCKED) == 0,
+      self.UNLOCKED == 1
+    )
+
+class Weapons:
+  SAR = Weapon(
+    Memory.SAR_AMMO, 
+    None, 
+    MemorySize.BIT0
+  )
+  MUON = Weapon(
+    Memory.MUON_PISTOL_AMMO, 
+    Memory.MUON_PISTOL_RESERVE_AMMO, 
+    MemorySize.BIT2
+  )
+  QUANTA = Weapon(
+    Memory.QUANTA_RIFLE_AMMO, 
+    Memory.QUANTA_RIFLE_RESERVE_AMMO, 
+    MemorySize.BIT3
+  )
+  FERMION = Weapon(
+    Memory.FERMION_SNIPER_AMMO, 
+    Memory.FERMION_SNIPER_RESERVE_AMMO, 
+    MemorySize.BIT4
+  )
+  LEPTON = Weapon(
+    Memory.LEPTON_SPREAD_AMMO, 
+    Memory.LEPTON_SPREAD_RESERVE_AMMO, 
+    MemorySize.BIT5
+  )
+  OXID = Weapon(
+    Memory.OXID_CANNON_AMMO, 
+    Memory.OXID_CANNON_RESERVE_AMMO, 
+    MemorySize.BIT6
+  )
+  SEEKER = Weapon(
+    Memory.SEEKER_PODS_AMMO, 
+    Memory.SEEKER_PODS_RESERVE_AMMO, 
+    MemorySize.BIT7
+  )
+
+
 def ptr(value):
   return tbyte(value)
+
+def clearedChapter(episode: int, map: str, subarea: str, gamemode = None, difficulty = None):
+  cond = group(
+    (Memory.CURRENT_EPISODE == episode),
+    (delta(Memory.END_SCREEN) == 0x00),
+    (Memory.END_SCREEN == 0x07),
+    (string_equals(Memory.CURRENT_AREA_ID, map, 2, endianness='little')),
+    (string_equals(Memory.CURRENT_SUBMAP_ID, subarea, 3, endianness='little'))
+  )
+  if(gamemode is not None):
+    cond.append((Memory.CURRENT_GAMEMODE == gamemode))
+  if(difficulty is not None):
+    cond.append(Memory.CURRENT_DIFFICULTY == difficulty)
+  return cond
--- Moon/set.py
+++ Moon/set.py
@@ -21,6 +21,72 @@ class MoonSet(AchievementSet):
             game_id=9969,
             title="Moon"
         )
+    
+    @achievement(631253)
+    def prologue_clear(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_00, 'a0', '000', gamemode=Gamemode.ADVENTURE))
+    
+    @achievement(631254)
+    def pss1_clear(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_02, 'a0', '025', gamemode=Gamemode.ADVENTURE))
+    
+    @achievement(631255)
+    def pss2_enter(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_03, 'a1', '000', gamemode=Gamemode.ADVENTURE))
+    
+    @achievement(631256)
+    def pss2_clear(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_05, 'a1', '016', gamemode=Gamemode.ADVENTURE))
+    
+    @achievement(631257)
+    def eteo_clear(self, ach: Achievement):
+        # for some reason this boss tps you to map 'dummy2' on clear instead of the next map for the LOLA section
+        # it doesn't follow the standard map naming convention (xx_yyy) they use, so it looks weird but it works 
+        ach.add_core(clearedChapter(Episode.EPISODE_07, 'du', 'my2', gamemode=Gamemode.ADVENTURE))
+    
+    @achievement(631258)
+    def waste_clear(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_09, 'a2', '022', gamemode=Gamemode.ADVENTURE))
+
+    @achievement(631259)
+    def powerstation_clear(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_11, 'a3', '032', gamemode=Gamemode.ADVENTURE))
+    
+    @achievement(631260)
+    def coldprocess_clear(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_13, 'a4', '016', gamemode=Gamemode.ADVENTURE))
+    
+    @achievement(631261)
+    def irradiated_clear(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_15, 'a1', '017', gamemode=Gamemode.ADVENTURE))
+    
+    @achievement(631262)
+    def overlord_clear(self, ach: Achievement):
+        ach.add_core(clearedChapter(Episode.EPISODE_17, 'a6', '014', gamemode=Gamemode.ADVENTURE))
+
+    @achievement(631263)
+    def muon_unlock(self, ach: Achievement):
+        ach.add_core(Weapons.MUON.unlocked(Episode.EPISODE_01))
+    
+    @achievement(631264)
+    def quanta_unlock(self, ach: Achievement):
+        ach.add_core(Weapons.QUANTA.unlocked(Episode.EPISODE_04))
+    
+    @achievement(631265)
+    def fermion_unlock(self, ach: Achievement):
+        ach.add_core(Weapons.FERMION.unlocked(Episode.EPISODE_10))
+    
+    @achievement(631266)
+    def lepton_unlock(self, ach: Achievement):
+        ach.add_core(Weapons.LEPTON.unlocked(Episode.EPISODE_12))
+    
+    @achievement(631267)
+    def oxid_unlock(self, ach: Achievement):
+        ach.add_core(Weapons.OXID.unlocked(Episode.EPISODE_14))
+
+    @achievement(631268)
+    def seeker_unlock(self, ach: Achievement):
+        ach.add_core(Weapons.SEEKER.unlocked(Episode.EPISODE_14))
 
 if __name__=="__main__":
     MoonSet().save("output/")
